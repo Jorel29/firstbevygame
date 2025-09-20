28ed73e2309767d3ac090dc46d936a35ead6c926
Jorel Huerto <jjhue29@gmail.com>
2025-09-20 00:22:14 -0700

    first entities with components and query

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
+use bevy::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
+}
+
+fn hello_world(){
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands){
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("John Rubicon".to_string())));
+    commands.spawn((Person, Name("Maxwell Sting".to_string())));
+    commands.spawn((Person, Name("Rapy Lace".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>){
+    for name in &query{
+        println!("hello {}!", name.0);
+    }
 }
